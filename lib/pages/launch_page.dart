752f5368cbd7a4fb625c07ee4cf33cf1ffd0f07d
Mayumi-Nakabayashi <mayumifinance@gmail.com>
2022-12-06 23:03:18 +0900

    背景画像を指定カラーに変更

--- lib/pages/launch_page.dart
+++ lib/pages/launch_page.dart
@@ -1,1 +1,22 @@
-//
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:recommender_app/firebase_options.dart';
+
+
+class LaunchPage extends StatefulWidget {
+  const LaunchPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LaunchPage> createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xff288776),
+    );
+  }
+}
