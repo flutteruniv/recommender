608e21c5c9fa2aa3e4abf010651f8b41f347ce7e
Mayumi-Nakabayashi <mayumifinance@gmail.com>
2022-12-07 19:34:14 +0900

    不要なコード削除

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LaunchPage(title: 'リコメンだー'),
+      home: const LaunchPage(),
     );
   }
 }
