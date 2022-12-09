17d1ff44f166eff8b7fbadc4230aa8e80ff9a513
Mayumi-Nakabayashi <mayumifinance@gmail.com>
2022-12-09 18:41:17 +0900

    画像設置、調整

--- lib/pages/launch_page.dart
+++ lib/pages/launch_page.dart
@@ -8,11 +8,42 @@ class LaunchPage extends StatefulWidget {
   State<LaunchPage> createState() => _LaunchPageState();
 }
 
+
 class _LaunchPageState extends State<LaunchPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff288776),
+    return Scaffold(
+      backgroundColor: const Color(0xff288776),
+      body: Center(
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            Positioned(
+              top: 50,
+              child: SizedBox(
+                  width: 280,
+                  child: Image.asset('images/logo.png')
+              ),
+            ),
+            Positioned(
+              child: SizedBox(
+                  width: 330,
+                  child: Image.asset('images/santa.png')
+              ),
+            ),
+            Positioned(
+              top: 300,
+              width: 250,
+              height: 250,
+              child: SizedBox(
+                  height: 10,
+                  width: 10,
+                  child: Image.asset('images/presentbox.png')
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
