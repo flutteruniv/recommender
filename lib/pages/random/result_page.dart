f98e5d9ef249a9a44b8b8c7467177d829f6540a1
yui <y129may9th@gmail.com>
2022-12-15 13:33:25 +0900

    chore: adjust width for web

--- lib/pages/random/result_page.dart
+++ lib/pages/random/result_page.dart
@@ -17,198 +17,203 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HexColor('EDE8E4'),
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('images/background-result.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(36),
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            item.name != null
-                                ? item.name!.replaceAll('\\n', '\n')
-                                : "",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                              height: 1.4,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 4),
-                          SizedBox(
-                            width: 135,
-                            height: 135,
-                            child: Image(
-                              image: NetworkImage(
-                                item.itemImage ?? "",
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            item.cost != '' ? '¥${item.cost}' : "",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              height: 1.4,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          SizedBox(
-                            width: 123,
-                            height: 36,
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                foregroundColor: Colors.white,
-                                backgroundColor: HexColor('288776'),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                              onPressed: () {},
-                              child: const Text(
-                                'Link',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 34),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: HexColor('FAF8F4'),
-                        borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(36),
-                          bottomRight: Radius.circular(36),
-                        ),
-                      ),
-                      child: Container(
-                        transform: Matrix4.translationValues(0.0, -30.0, 0.0),
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20.0,
-                        ),
+      body: Center(
+        child: Container(
+          width: 390,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('images/background-result.png'),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(36),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
                         child: Column(
                           children: [
-                            Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: NetworkImage(item.memberIcon ?? ""),
-                                ),
-                                border: Border.all(
-                                  width: 3,
-                                  color: HexColor('F4F4F3'),
-                                ),
+                            Text(
+                              item.name != null
+                                  ? item.name!.replaceAll('\\n', '\n')
+                                  : "",
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                height: 1.4,
                               ),
-                            ),
-                            Container(
-                              transform:
-                                  Matrix4.translationValues(0.0, -8.0, 0.0),
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0,
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(14),
-                              ),
-                              child: Text(
-                                item.memberName ?? "",
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                ),
-                              ),
+                              textAlign: TextAlign.center,
                             ),
                             const SizedBox(height: 4),
+                            SizedBox(
+                              width: 135,
+                              height: 135,
+                              child: Image(
+                                image: NetworkImage(
+                                  item.itemImage ?? "",
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 8),
                             Text(
-                              'おすすめコメント',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: HexColor('288776'),
+                              item.cost != '' ? '¥${item.cost}' : "",
+                              style: const TextStyle(
                                 fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                height: 1.4,
                               ),
                             ),
                             const SizedBox(height: 8),
                             SizedBox(
-                              height: 63,
-                              child: Text(
-                                // note: 12文字 x 3行
-                                item.memberMessage ?? "",
-                                style: const TextStyle(
-                                  fontSize: 13,
+                              width: 123,
+                              height: 36,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: HexColor('288776'),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
                                 ),
-                                textAlign: TextAlign.center,
+                                onPressed: () {
+                                  onLaunchUrl();
+                                },
+                                child: const Text(
+                                  'Link',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ),
                             ),
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(height: 34),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: HexColor('FAF8F4'),
+                          borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(36),
+                            bottomRight: Radius.circular(36),
+                          ),
+                        ),
+                        child: Container(
+                          transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                          ),
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 60,
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(item.memberIcon ?? ""),
+                                  ),
+                                  border: Border.all(
+                                    width: 3,
+                                    color: HexColor('F4F4F3'),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                transform:
+                                    Matrix4.translationValues(0.0, -8.0, 0.0),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 8.0,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(14),
+                                ),
+                                child: Text(
+                                  item.memberName ?? "",
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                'おすすめコメント',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: HexColor('288776'),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              SizedBox(
+                                height: 63,
+                                child: Text(
+                                  // note: 12文字 x 3行
+                                  item.memberMessage ?? "",
+                                  style: const TextStyle(
+                                    fontSize: 13,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 12),
-            SizedBox(
-              width: 214,
-              height: 56,
-              child: OutlinedButton(
-                onPressed: () {
-                  final item = getRandomItem();
-                  Navigator.push(
-                    context,
-                    CustomPageRoute(
-                      ShortFlip(item: item, items: items),
+              const SizedBox(height: 12),
+              SizedBox(
+                width: 214,
+                height: 56,
+                child: OutlinedButton(
+                  onPressed: () {
+                    final item = getRandomItem();
+                    Navigator.push(
+                      context,
+                      CustomPageRoute(
+                        ShortFlip(item: item, items: items),
+                      ),
+                    );
+                  },
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  );
-                },
-                style: OutlinedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+                    side: BorderSide(
+                      width: 1,
+                      color: HexColor('94D4C8'),
+                    ),
                   ),
-                  side: BorderSide(
-                    width: 1,
-                    color: HexColor('94D4C8'),
-                  ),
-                ),
-                child: Text(
-                  '←選びなおす',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: HexColor('288776'),
+                  child: Text(
+                    '←選びなおす',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: HexColor('288776'),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
